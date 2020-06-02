951b5c2a603f250119f836a942919711791eda3e
dudwl901 <dudwls901@naver.com>
2020-06-02 21:39:20 +0900

    김영진이바꾼다

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //머러움랑
+        //김영진이 바꿔요
     }
 }
