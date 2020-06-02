3f0f145f5614166035fd2ad8cc23839953c9c51b
dudwl901 <dudwls901@naver.com>
2020-06-03 01:18:24 +0900

    내 브런치에서 올린 걸 마스터에서

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         //김영진이 바꿔요
         //ㅋㅇㄻㅇㅁㄹㅇㅁㄹ
         //내 브런치에서 올린 걸 마스터에서 해보자
+        //내 브런치에서 커밋 후 로컬 마스터에 merge 후 로컬 마스터에서 리모트 마스터로 푸시푸시
     }
 }
