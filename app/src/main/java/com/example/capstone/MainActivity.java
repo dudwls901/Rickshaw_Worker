74eb8cd1b624335307347ac5e9c15640958f1ec9
dudwl901 <dudwls901@naver.com>
2020-06-03 17:06:37 +0900

    ㅋㄴㅇㄹㅇㅋㄹ

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         //내 브런치에서 올린 걸 마스터에서 해보자
         //내 브런치에서 커밋 후 로컬 마스터에 merge 후 로컬 마스터에서 리모트 마스터로 푸시푸시
         // 나는 바꾸고 있는데 누가 수정을 했다네
-
+//캉아아아아아아
         //안알려준다잉
     }
 }
