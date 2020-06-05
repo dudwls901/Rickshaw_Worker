f28c7db42702cef36696adc28797f289261c07d9
soyeon-829 <jenny1920@naver.com>
2020-06-05 22:36:04 +0900

    폴더구조완료

--- app/src/main/java/kr/co/ilg/activity/myfield/MyApplyStateActivity.java
+++ app/src/main/java/kr/co/ilg/activity/myfield/MyApplyStateActivity.java
@@ -7,7 +7,7 @@ import androidx.annotation.Nullable;
 
 import com.example.capstone.R;
 
-public class ApplyStateActivity extends Activity {
+public class MyApplyStateActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
