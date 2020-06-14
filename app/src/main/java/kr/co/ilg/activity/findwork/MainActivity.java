2071721ff818be171aea4350de4c4c71338ba8f1
soyeon-829 <jenny1920@naver.com>
2020-06-14 15:35:38 +0900

    폴더구조완료

--- app/src/main/java/kr/co/ilg/activity/findwork/MainActivity.java
+++ app/src/main/java/kr/co/ilg/activity/findwork/MainActivity.java
@@ -12,14 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {//1
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//머러움랑
-        //김영진이 바꿔요
-        //ㅋㅇㄻㅇㅁㄹㅇㅁㄹ
-        //내 브런치에서 올린 걸 마스터에서 해보자
-        //내 브런치에서 커밋 후 로컬 마스터에 merge 후 로컬 마스터에서 리모트 마스터로 푸시푸시
-        // 나는 바꾸고 있는데 누가 수정을 했다네
-//캉아아아아아아
-        //안알려준다잉
-        // 동연에서 커밋앤 푸시
+        setTitle("일자리 찾기");
+
     }
 }
