ae9a653543e494b50a80629861c08bc3f1eed9ef
soyeon-829 <jenny1920@naver.com>
2020-06-14 14:50:17 +0900

    폴더구조완료

---
+++ app/src/main/java/kr/co/ilg/activity/login/LoginActivity.java
@@ -0,0 +1,17 @@
+package kr.co.ilg.activity.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.capstone.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
