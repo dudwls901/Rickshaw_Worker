28ea0b29c56f6c2c21b853c75982deeb5df8e878
dudwl901 <dudwls901@naver.com>
2020-06-14 23:13:39 +0900

    ㅋㄴㅇㄹㅇㅋㄹ

--- app/src/main/java/com/example/capstone/MainActivity.java
+++ app/src/main/java/com/example/capstone/MainActivity.java
@@ -1,17 +1,40 @@
 package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+<<<<<<< Updated upstream:app/src/main/java/com/example/capstone/MainActivity.java
+=======
+import com.example.capstone.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+>>>>>>> Stashed changes:app/src/main/java/kr/co/ilg/activity/findwork/MainActivity.java
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {//1
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+<<<<<<< Updated upstream:app/src/main/java/com/example/capstone/MainActivity.java
 //머러움랑
         //김영진이 바꿔요
         //ㅋㅇㄻㅇㅁㄹㅇㅁㄹ
+=======
+//        BottomNavigationView navView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_home, R.id.navigation_myfield, R.id.navigation_myinfo)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
+
+>>>>>>> Stashed changes:app/src/main/java/kr/co/ilg/activity/findwork/MainActivity.java
     }
 }
