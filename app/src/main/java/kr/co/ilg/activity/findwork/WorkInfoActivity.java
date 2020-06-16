071e8d131dd5fd319e7383e984083e5095ef9fb1
soyeon-829 <jenny1920@naver.com>
2020-06-16 22:51:47 +0900

    20200616 22:51

--- app/src/main/java/kr/co/ilg/activity/findwork/WorkInfoActivity.java
+++ app/src/main/java/kr/co/ilg/activity/findwork/WorkInfoActivity.java
@@ -2,7 +2,12 @@ package kr.co.ilg.activity.findwork;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +15,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.capstone.R;
 
 public class WorkInfoActivity extends AppCompatActivity {
-
+    TextView field_address,office_name;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.work_info);
         //ActionBar ad=getActionBar();
         //ad.setDisplayHomeAsUpEnabled(true);
+
+        field_address=findViewById(R.id.place);
+        office_name=findViewById(R.id.office_info);
+        field_address.setPaintFlags(field_address.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+        office_name.setPaintFlags(field_address.getPaintFlags()| Paint.UNDERLINE_TEXT_FLAG);
+
+        field_address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(WorkInfoActivity.this,FieldInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        office_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(WorkInfoActivity.this,OfficeInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
