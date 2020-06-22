6ee48208c05c6783f201705dbbc7e15cff13f37f
dudwl901 <dudwls901@naver.com>
2020-06-22 17:21:36 +0900

    17:21커밋

--- app/src/main/java/kr/co/ilg/activity/findwork/WorkInfoActivity.java
+++ app/src/main/java/kr/co/ilg/activity/findwork/WorkInfoActivity.java
@@ -6,24 +6,42 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.capstone.R;
 
 public class WorkInfoActivity extends AppCompatActivity {
     TextView field_address,office_name;
     Button map_btn;
+    Toolbar toolbar;
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home: {
+                // 해당 버튼을 눌렀을 때 적절한 액션을 넣는다. return true;
+            finish();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.work_info);
         //ActionBar ad=getActionBar();
         //ad.setDisplayHomeAsUpEnabled(true);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         field_address=findViewById(R.id.place);
         office_name=findViewById(R.id.office_info);
