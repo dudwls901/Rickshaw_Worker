2c85a2d25954ff81b0597ffe5f1981a461c36c9e
정선우 <miney8332@naver.com>
2020-06-23 22:07:14 +0900

    확인용                 ㅡ선우ㅡ

--- app/src/main/java/kr/co/ilg/activity/mypage/AccountManageActivity.java
+++ app/src/main/java/kr/co/ilg/activity/mypage/AccountManageActivity.java
@@ -1,7 +1,10 @@
 package kr.co.ilg.activity.mypage;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
@@ -13,5 +16,14 @@ public class AccountManageActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accountmanage);
+
+        Button accountmodify = (Button) findViewById(R.id.accountmodify);
+
+        accountmodify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),AccountAddActivity.class); startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/kr/co/ilg/activity/mypage/mypagereviewitem.java
@@ -0,0 +1,12 @@
+package kr.co.ilg.activity.mypage;
+
+
+public class mypagereviewitem {
+    public String mypagereviewname, mypagereview, mypagereviewdate;
+
+    public mypagereviewitem(String mypagereviewname,  String mypagereview,String mypagereviewdate){
+        this.mypagereviewname = mypagereviewname;
+        this.mypagereview = mypagereview;
+        this.mypagereviewdate = mypagereviewdate;
+    }
+}
