cf5f75edbbe68d307f335459a2e1ed2232ab94be
정선우 <miney8332@naver.com>
2020-10-08 13:19:49 +0900

    회원가입(이메일 인증, 직종선택, 지역선택 등등)

--- app/src/main/java/kr/co/ilg/activity/login/SignupEmailActivity.java
+++ app/src/main/java/kr/co/ilg/activity/login/SignupEmailActivity.java
@@ -1,36 +1,96 @@
 package kr.co.ilg.activity.login;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.capstone.R;
 
 public class SignupEmailActivity extends AppCompatActivity {
 
-    Button sendCodeBtn, nextBtn;
+    Button sendCodeBtn, nextBtn,codeBtn;
+    String Code;
+    EditText emailET, codeET;
+    boolean sign=false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_email);
 
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .permitDiskReads()
+                .permitDiskWrites()
+                .permitNetwork().build());
+
         sendCodeBtn = findViewById(R.id.sendCodeBtn);
         nextBtn = findViewById(R.id.nextBtn);
+        emailET = findViewById(R.id.emailET);
+        codeET = findViewById(R.id.codeET);
+        codeBtn = findViewById(R.id.codeBtn);
 
         sendCodeBtn.setText("인증번호\n발송");
 
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignupEmailActivity.this, SignupPasswordActivity.class);
-                startActivity(intent);
+               if (sign == true ) {
+
+                    Intent intent = new Intent(SignupEmailActivity.this, SignupPasswordActivity.class);
+                    startActivity(intent);
+                }
+                else{Toast.makeText(SignupEmailActivity.this, "인증 완료해주십시오", Toast.LENGTH_SHORT).show();}
             }
         });
+        codeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Code.equals(codeET.getText().toString()) ) {
+                    Toast.makeText(SignupEmailActivity.this, "인증 완료", Toast.LENGTH_SHORT).show();
+                    codeBtn.setEnabled(false);
+                    sign = true;
+                }
+                else{
+                    Toast.makeText(SignupEmailActivity.this, "인증 오류입니다", Toast.LENGTH_SHORT).show();
+                    sign = false;
+                }
+
+            }
+        });
+
+        sendCodeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GmailSender sender = new GmailSender("sun83324@gmail.com", "wjdtjsdn2");
+                Code = sender.getEmailCode();
+
+                if (android.os.Build.VERSION.SDK_INT > 9) {
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+                }
+                try {
+                    sender.sendMail("인력거 인증번호 알림", "인증번호 : "+Code , emailET.getText().toString());// 넣을것
+                    Toast.makeText(SignupEmailActivity.this, "발송 완료", Toast.LENGTH_SHORT).show();
+                    codeBtn.setEnabled(true);
+                }
+                catch (Exception e)
+                {
+                    Log.e("SendMail", e.getMessage(), e);
+                    Toast.makeText(SignupEmailActivity.this, "올바른 메일을 입력해주십시오", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 }
+
--- app/src/main/java/kr/co/ilg/activity/mypage/JobSelectActivity.java
+++ app/src/main/java/kr/co/ilg/activity/mypage/JobSelectActivity.java
@@ -16,57 +16,72 @@ import com.example.capstone.R;
 
 import org.w3c.dom.Text;
 
-public class JobSelectActivity extends AppCompatActivity {
+public class JobSelectActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button yong, botong;
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10,btn11,btn12,btn13,btn14,btn15,btn16;
+    Button[] job ={btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10,btn11,btn12,btn13,btn14,btn15,btn16};
+    int[] jobid = {R.id.btn1,R.id.btn2,R.id.btn3,R.id.btn4,R.id.btn5,R.id.btn6,R.id.btn7,R.id.btn8,R.id.btn9,R.id.btn10,R.id.btn11,
+            R.id.btn12,R.id.btn13,R.id.btn14,R.id.btn15,R.id.btn16};
+    int check[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     TextView sltTV;
+    String jobcheck="",jobs="";
     int btnFlag = 0;
     int btnFlag2 = 0;
+    int i,j=0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.job_select);
 
-        yong = findViewById(R.id.yong);
-        botong = findViewById(R.id.botong);
-        sltTV = findViewById(R.id.sltTV);
+        for( i=0; i<16; i++){
+            job[i] = (Button)findViewById(jobid[i]);
+            job[i].setOnClickListener(this);  // 직업버튼 인플레이션
+        }
+            sltTV = (TextView) findViewById(R.id.sltTV);
+
 
         findViewById(R.id.okBtn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(JobSelectActivity.this, CareerActivity.class);
+                intent.putExtra("jobs",jobs);
                 startActivity(intent);
             }
-        });
-        yong.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(btnFlag == 0) {
-                    yong.setBackground(getDrawable(R.drawable.custom_btn_mainclr));
-                    sltTV.setText(sltTV.getText().toString() + "  용접공");
-                    btnFlag = 1;
-                }
-                else {
-                    yong.setBackground(getDrawable(R.drawable.custom_btn_lightclr));
-                    btnFlag = 0;
-                }
-            }
-        });
-        botong.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(btnFlag2 == 0) {
-                    botong.setBackground(getDrawable(R.drawable.custom_btn_mainclr));
-                    sltTV.setText(sltTV.getText().toString() + "  보통인부");
-                    btnFlag2 = 1;
-                }
-                else {
-                    botong.setBackground(getDrawable(R.drawable.custom_btn_lightclr));
-                    btnFlag2 = 0;
-                }
-            }
-        });
+        }); // 확인버튼 인플레이션 및 intent 구현
 
     }
+    @Override
+    public void onClick(View v) {
+        jobs = "";
+        for (int k = 0; k < 16; k++) {
+            if(jobid[k]==v.getId()) {
+                if (check[k] == 0) {
+                    j+=1;
+                    job[k].setBackground(getDrawable(R.drawable.custom_btn_mainclr));
+                    check[k]=1;
+                } else {
+                    j-=1;
+                    job[k].setBackground(getDrawable(R.drawable.custom_btn_lightclr)); // wrap 텍스트뷰 3개 만들어서 상단바 제어
+                    check[k]=0;
+                }
+            }
+
+            if(check[k]==1) jobs = job[k].getText().toString() + "  " +jobs;
+
+        }
+        sltTV.setText(jobs);
+        if(j==3) {
+            for(int i=0; i<16; i++){
+                if(check[i]==0) job[i].setEnabled(false);
+                //토스트메세지까지
+            }
+        }
+        else {
+            for (int i = 0; i < 16; i++) {
+                if (check[i] == 0) job[i].setEnabled(true);
+            }
+        }
+    }
+
 }
