28f83d9b355031f6599e74fc29b96a0e9e2ebbdd
정선우 <miney8332@naver.com>
2020-10-18 15:10:18 +0900

    로그인시 사용자 정보 맵핑, mypage 사용자정보 띄우기

--- app/src/main/java/kr/co/ilg/activity/mypage/MyInfomanageActivity.java
+++ app/src/main/java/kr/co/ilg/activity/mypage/MyInfomanageActivity.java
@@ -1,18 +1,40 @@
 package kr.co.ilg.activity.mypage;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 import com.example.capstone.R;
 
+import org.w3c.dom.Text;
+
+import kr.co.ilg.activity.findwork.Sharedpreference;
+
 public class MyInfomanageActivity extends Activity {
+
+    private Context mContext;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myinfomanage);
+
+        mContext = this;
+
+        TextView myname = findViewById(R.id.myname);
+        TextView age = findViewById(R.id.age);
+        TextView phonenum = findViewById(R.id.phonenum);
+        TextView email = findViewById(R.id.email);
+        TextView introduce = findViewById(R.id.introduce);
+
+        myname.setText(Sharedpreference.get_Nickname(mContext, "nickname"));
+        age.setText(Sharedpreference.get_Birth(mContext, "worker_birth"));
+        phonenum.setText(Sharedpreference.get_Phonenum(mContext, "worker_phonenum"));
+        email.setText(Sharedpreference.get_email(mContext, "email"));
+        introduce.setText(Sharedpreference.get_introduce(mContext,"worker_introduce"));
 
     }
 }
--- app/src/main/kotlin/com/example/capstone/MainActivity.kt
+++ app/src/main/kotlin/com/example/capstone/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.capstone
 import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
 import android.widget.*
@@ -9,6 +12,7 @@ import com.android.volley.toolbox.Volley
 import com.kakao.sdk.auth.LoginClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.AuthErrorCause.*
+import com.kakao.sdk.user.UserApiClient
 import kr.co.ilg.activity.findwork.MainActivity
 import kr.co.ilg.activity.login.FindPasswordInfoActivity
 import kr.co.ilg.activity.login.SignupEmailActivity
@@ -28,10 +32,22 @@ class MainActivity : Activity() {
         val loginBtn = findViewById<Button>(R.id.loginBtn)
          val findPwBtn = findViewById<TextView>(R.id.findPwBtn)
          val signUpBtn = findViewById<TextView>(R.id.signUpBtn)
-         fun intent1(){
+
+         fun intent1(workeremail:String, workername:String,worker_gender:String, worker_birth:String, worker_phonenum:String, worker_bankaccount:String, worker_bankname:String, worker_introduce:String){
              var intent = Intent(this, MainActivity::class.java)
-             startActivity(intent)
+             intent.putExtra("email",workeremail)
+             intent.putExtra("nickname",workername)
+             intent.putExtra("worker_gender",worker_gender)
+             intent.putExtra("worker_birth",worker_birth)
+             intent.putExtra("worker_phonenum",worker_phonenum)
+             intent.putExtra("worker_bankaccount",worker_bankaccount)
+             intent.putExtra("worker_bankname",worker_bankname)
+             intent.putExtra("worker_introduce",worker_introduce)
+
+             startActivity(intent) // 일반로그인 정보갖고오기
+
          }
+
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
                 when {
@@ -67,7 +83,67 @@ class MainActivity : Activity() {
             else if (token != null) {
                 Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+
+
+                /*UserApiClient.instance.me { user, error ->
+                    if (error != null) {
+                        Log.d(TAG, "사용자 정보 요청 실패", error)
+                    }
+                    else if (user != null) {
+                        Log.d(TAG, "사용자 정보 요청 성공" +
+                                "\n이메일: ${user.kakaoAccount?.email}")
+                    }
+                }*/
+
+                UserApiClient.instance.me { user, error ->
+                    var typeof_email:Boolean = false
+                    if (error != null) {
+                        Log.e(TAG, "사용자 정보 요청 실패", error)
+                    }
+                    else if (user != null) {
+                        if (user.kakaoAccount?.email != null) {
+                            Log.i(TAG, "이메일: ${user.kakaoAccount?.email}"+
+                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" )
+                            intent.putExtra("email",user.kakaoAccount?.email)
+                            intent.putExtra("nickname",user.kakaoAccount?.profile?.nickname)
+                            intent.putExtra("typeof_email",typeof_email)
+                            startActivity(intent)
+                        }
+                        else if (user.kakaoAccount?.emailNeedsAgreement == false) {
+                            Log.e(TAG, "사용자 계정에 이메일 없음.")
+                            intent.putExtra("memberid",user.id)
+                            intent.putExtra("nickname",user.kakaoAccount?.profile?.nickname)
+                            intent.putExtra("typeof_email",true)
+                            startActivity(intent)
+                        }
+                        else if (user.kakaoAccount?.emailNeedsAgreement == true) {
+                            Log.d(TAG, "사용자에게 이메일 제공 동의를 받아야함.")
+
+                            val scopes = listOf("account_email")
+                            LoginClient.instance.loginWithNewScopes(this, scopes) { token, error ->
+                                if (error != null) {
+                                    Log.e(TAG, "이메일 제공 동의 실패", error)
+                                } else {
+                                    Log.d(TAG, "allowed scopes: ${token!!.scopes}")
+
+                                    UserApiClient.instance.me { user, error ->
+                                        if (error != null) {
+                                            Log.e(TAG, "사용자 정보 요청 실패", error)
+                                        }
+                                        else if (user != null) {
+                                            Log.i(TAG, "이메일: ${user.kakaoAccount?.email}"+
+                                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" )
+                                            intent.putExtra("email",user.kakaoAccount?.email)
+                                            intent.putExtra("nickname",user.kakaoAccount?.profile?.nickname)
+                                            intent.putExtra("typeof_email",typeof_email)
+                                            startActivity(intent)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
         kakaoLoginBtn.setOnClickListener {
@@ -89,10 +165,15 @@ class MainActivity : Activity() {
                          val jResponse = JSONObject(response!!.substring(response!!.indexOf("{"), response!!.lastIndexOf("}") + 1))
                          val isExistWorker = jResponse.getBoolean("tryLogin")
                          if (isExistWorker) {  // 회원이 존재하면
-
-                             val worker_email = jResponse.getString("worker_email")
-                             intent1()
-
+                             var worker_email1 = jResponse.getString("worker_email")
+                             var worker_name = jResponse.getString("worker_name")
+                             var worker_gender = jResponse.getString("worker_gender")
+                             var worker_birth = jResponse.getString("worker_birth")
+                             var worker_phonenum = jResponse.getString("worker_phonenum")
+                             var worker_bankaccount = jResponse.getString("worker_bankaccount")
+                             var worker_bankname = jResponse.getString("worker_bankname")
+                             var worker_introduce = jResponse.getString("worker_introduce")
+                             intent1(worker_email1,worker_name, worker_gender, worker_birth, worker_phonenum, worker_bankaccount, worker_bankname, worker_introduce)
                              //Toast.makeText(FindPasswordInfoActivity.this, "등록된 "+worker_pw, Toast.LENGTH_SHORT).show();
                          } else {  // 회원이 존재하지 않는다면
                              Toast.makeText(this@MainActivity,"없는 이메일입니다.",Toast.LENGTH_SHORT).show()
